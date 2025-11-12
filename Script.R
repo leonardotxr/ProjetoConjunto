f6d8eedf38406ce0b34a05d3036f7800294831e7
Aluno <leonardocteixeira02@gmail.com>
2025-11-12 10:32:28 -0300

    inserindo segundo grafico

--- Script.R
+++ Script.R
@@ -4,11 +4,16 @@ library(dplyr)
 
 Boxplot_trabalho = ggplot(Base_Casos, aes(y = Estatura, fill = "red")) +
   geom_boxplot() +
+  labs(title = "Boxplot Estatura",
+       y = "Estatura (em metros)") +
   theme_classic()
 
 
-
+Boxplot_trabalho2 = ggplot(Base_Casos, aes(y = Peso, fill = "red")) +
+  geom_boxplot() +
+  labs( title = "Boxplot Peso",
+        y = "Peso (em kg)") +
+  theme_classic()
 
            
-    
-  
+
